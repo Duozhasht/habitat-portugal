fab1e7a2a7aadd672da30e7cdac502f0d8829783
Davide Silva <davidealbertopintosilva@gmail.com>
2014-12-18 12:49:02 +0000

    Add Voluntario model and Repository

---
+++ habitat-portugal/src/Model/Voluntario.java
@@ -0,0 +1,225 @@
+package Model;
+
+/**
+ * @author davide on 18/12/14.
+ */
+
+@SuppressWarnings("UnusedDeclaration")
+public class Voluntario {
+    private int id_voluntario = -1;
+    private String nome_voluntario;
+    private String data_nascimento;
+    private String profissao;
+    private String morada;
+    private String codigo_postal;
+    private String localidade;
+    private String telefone;
+    private String telemovel;
+    private String email;
+    private String hab_academ;
+    private String conhec_lingui;
+    private String form_compl;
+    private String exp_voluntariado;
+    private String conhec_constr;
+    private String trabalho_grupo;
+    private String pub;
+    private String disponi_tempo;
+    private String cca_habitat;
+
+    public Voluntario() {
+        this.nome_voluntario = "";
+        this.data_nascimento = "";
+        this.profissao = "";
+        this.morada = "";
+        this.codigo_postal = "";
+        this.localidade = "";
+        this.telefone = "";
+        this.telemovel = "";
+        this.email = "";
+        this.hab_academ = "";
+        this.conhec_lingui = "";
+        this.form_compl = "";
+        this.exp_voluntariado = "";
+        this.conhec_constr = "";
+        this.trabalho_grupo = "";
+        this.pub = "";
+        this.disponi_tempo = "";
+        this.cca_habitat = "";
+    }
+
+    public Voluntario(String nome_voluntario, String data_nascimento, String profissao, String morada,
+                      String codigo_postal, String localidade, String telefone, String telemovel, String email,
+                      String hab_academ, String conhec_lingui, String form_compl, String exp_voluntariado,
+                      String conhec_constr, String trabalho_grupo, String pub, String disponi_tempo, String cca_habitat) {
+        this.nome_voluntario = nome_voluntario;
+        this.data_nascimento = data_nascimento;
+        this.profissao = profissao;
+        this.morada = morada;
+        this.codigo_postal = codigo_postal;
+        this.localidade = localidade;
+        this.telefone = telefone;
+        this.telemovel = telemovel;
+        this.email = email;
+        this.hab_academ = hab_academ;
+        this.conhec_lingui = conhec_lingui;
+        this.form_compl = form_compl;
+        this.exp_voluntariado = exp_voluntariado;
+        this.conhec_constr = conhec_constr;
+        this.trabalho_grupo = trabalho_grupo;
+        this.pub = pub;
+        this.disponi_tempo = disponi_tempo;
+        this.cca_habitat = cca_habitat;
+    }
+
+    public int getId_voluntario() {
+        return id_voluntario;
+    }
+
+    public void setId_voluntario(int id_voluntario) {
+        this.id_voluntario = id_voluntario;
+    }
+
+    public String getNome_voluntario() {
+        return nome_voluntario;
+    }
+
+    public void setNome_voluntario(String nome_voluntario) {
+        this.nome_voluntario = nome_voluntario;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public String getCodigo_postal() {
+        return codigo_postal;
+    }
+
+    public void setCodigo_postal(String codigo_postal) {
+        this.codigo_postal = codigo_postal;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTelemovel() {
+        return telemovel;
+    }
+
+    public void setTelemovel(String telemovel) {
+        this.telemovel = telemovel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHab_academ() {
+        return hab_academ;
+    }
+
+    public void setHab_academ(String hab_academ) {
+        this.hab_academ = hab_academ;
+    }
+
+    public String getConhec_lingui() {
+        return conhec_lingui;
+    }
+
+    public void setConhec_lingui(String conhec_lingui) {
+        this.conhec_lingui = conhec_lingui;
+    }
+
+    public String getForm_compl() {
+        return form_compl;
+    }
+
+    public void setForm_compl(String form_compl) {
+        this.form_compl = form_compl;
+    }
+
+    public String getExp_voluntariado() {
+        return exp_voluntariado;
+    }
+
+    public void setExp_voluntariado(String exp_voluntariado) {
+        this.exp_voluntariado = exp_voluntariado;
+    }
+
+    public String getConhec_constr() {
+        return conhec_constr;
+    }
+
+    public void setConhec_constr(String conhec_constr) {
+        this.conhec_constr = conhec_constr;
+    }
+
+    public String getTrabalho_grupo() {
+        return trabalho_grupo;
+    }
+
+    public void setTrabalho_grupo(String trabalho_grupo) {
+        this.trabalho_grupo = trabalho_grupo;
+    }
+
+    public String getPub() {
+        return pub;
+    }
+
+    public void setPub(String pub) {
+        this.pub = pub;
+    }
+
+    public String getDisponi_tempo() {
+        return disponi_tempo;
+    }
+
+    public void setDisponi_tempo(String disponi_tempo) {
+        this.disponi_tempo = disponi_tempo;
+    }
+
+    public String getCca_habitat() {
+        return cca_habitat;
+    }
+
+    public void setCca_habitat(String cca_habitat) {
+        this.cca_habitat = cca_habitat;
+    }
+}
---
+++ habitat-portugal/src/Persistence/VoluntarioRepository.java
@@ -0,0 +1,324 @@
+package Persistence;
+
+import Model.Voluntario;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ * @author davide on 18/12/14.
+ */
+
+@SuppressWarnings("UnusedDeclaration")
+public class VoluntarioRepository implements Map<Integer, Voluntario> {
+    private static final String INSERT_VOLUNTARIO = "insert into voluntario (nome_voluntario, data_nascimento, profissao, morada, codigo_postal, localidade, telefone, telemovel, email, hab_academ, conhec_lingui, form_compl, exp_voluntariado, conhec_constr, trabalho_grupo, pub , disponi_tempo, cca_habitat) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    private static final String UPDATE_VOLUNTARIO = "update voluntario set nome_voluntario = ?, data_nascimento = ?, profissao = ?, morada = ?, codigo_postal = ?, localidade = ?, telefone = ?, telemovel = ?, email = ?, hab_academ = ?, conhec_lingui = ?, form_compl = ?, exp_voluntariado = ?, conhec_constr = ?, trabalho_grupo = ?, pub  = ?, disponi_tempo = ?, cca_habitat = ? where id_voluntario = ?";
+
+    private static final String SELECT_VOLUNTARIO = "select * from voluntario where id_voluntario = ?";
+    private static final String SELECT_VOLUNTARIOS = "select * from voluntario";
+
+    private static final String DELETE_VOLUNTARIO = "delete from voluntario where id_voluntario = ?";
+    private static final String DELETE_VOLUNTARIOS = "delete from voluntario";
+
+    private static final String COUNT_VOLUNTARIOS = "select count(*) as n from voluntario";
+    private static final String SELECT_IDS = "select id_voluntario from voluntario";
+
+    private final String url;
+    private final String user;
+    private final String password;
+
+    public VoluntarioRepository(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    @Override
+    public int size() {
+        try {
+            int count;
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+
+            try (ResultSet resultSet = statement.executeQuery(COUNT_VOLUNTARIOS)) {
+                if (resultSet.next())
+                    count = resultSet.getInt("n");
+                else
+                    count = -1;
+            } finally {
+                statement.close();
+                connection.close();
+            }
+
+            return count;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        try {
+
+            Connection connection = DriverManager.getConnection(url,user,password);
+            PreparedStatement statement = connection.prepareStatement(SELECT_VOLUNTARIO);
+
+            statement.setInt(1, (int) key);
+
+            try (ResultSet result = statement.executeQuery()) {
+                return result.next();
+            } finally {
+                statement.close();
+                connection.close();
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return containsKey(((Voluntario) value).getId_voluntario());
+    }
+
+    @Override
+    public Voluntario get(Object key) {
+        Voluntario voluntario = null;
+        try {
+
+            Connection connection = DriverManager.getConnection(url,user,password);
+            PreparedStatement statement = connection.prepareStatement(SELECT_VOLUNTARIO);
+
+            statement.setInt(1,(int) key);
+
+            try (ResultSet result = statement.executeQuery()) {
+                if (result.next()) {
+                    voluntario = new Voluntario();
+                    voluntario.setId_voluntario(result.getInt("id_voluntario"));
+                    voluntario.setNome_voluntario(result.getString("nome_voluntario"));
+                    voluntario.setData_nascimento(result.getString("data_nascimento"));
+                    voluntario.setProfissao(result.getString("profissao"));
+                    voluntario.setMorada(result.getString("morada"));
+                    voluntario.setCodigo_postal(result.getString("codigo_postal"));
+                    voluntario.setLocalidade(result.getString("localidade"));
+                    voluntario.setTelefone(result.getString("telefone"));
+                    voluntario.setTelemovel(result.getString("telemovel"));
+                    voluntario.setEmail(result.getString("email"));
+                    voluntario.setHab_academ(result.getString("hab_academ"));
+                    voluntario.setConhec_lingui(result.getString("conhec_lingui"));
+                    voluntario.setForm_compl(result.getString("form_compl"));
+                    voluntario.setExp_voluntariado(result.getString("exp_voluntariado"));
+                    voluntario.setConhec_constr(result.getString("conhec_constr"));
+                    voluntario.setTrabalho_grupo(result.getString("trabalho_grupo"));
+                    voluntario.setPub(result.getString("pub"));
+                    voluntario.setDisponi_tempo(result.getString("disponi_tempo"));
+                    voluntario.setCca_habitat(result.getString("cca_habitat"));
+                }
+            } finally {
+                statement.close();
+                connection.close();
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return voluntario;
+    }
+
+    @Override
+    public Voluntario put(Integer key, Voluntario value) {
+        String query;
+        int autoGeneratedKeys;
+        boolean isUpdate;
+        if (key < 0) {
+            isUpdate = false;
+            query = INSERT_VOLUNTARIO;
+            autoGeneratedKeys = Statement.RETURN_GENERATED_KEYS;
+        }
+        else {
+            isUpdate = true;
+            query = UPDATE_VOLUNTARIO;
+            autoGeneratedKeys = Statement.NO_GENERATED_KEYS;
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url,user,password);
+            PreparedStatement statement = connection.prepareStatement(query,autoGeneratedKeys);
+
+            statement.setString(1, value.getNome_voluntario());
+            statement.setString(2, value.getData_nascimento());
+            statement.setString(3, value.getProfissao());
+            statement.setString(4, value.getMorada());
+            statement.setString(5, value.getCodigo_postal());
+            statement.setString(6, value.getLocalidade());
+            statement.setString(7, value.getTelefone());
+            statement.setString(8, value.getTelemovel());
+            statement.setString(9, value.getEmail());
+            statement.setString(10, value.getHab_academ());
+            statement.setString(11, value.getConhec_lingui());
+            statement.setString(12, value.getForm_compl());
+            statement.setString(13, value.getExp_voluntariado());
+            statement.setString(14, value.getConhec_constr());
+            statement.setString(15, value.getTrabalho_grupo());
+            statement.setString(16, value.getPub());
+            statement.setString(17, value.getDisponi_tempo());
+            statement.setString(18, value.getCca_habitat());
+
+            if (isUpdate) {
+                statement.setInt(19,key);
+            }
+
+            statement.executeUpdate();
+
+            try {
+                if (autoGeneratedKeys == Statement.RETURN_GENERATED_KEYS) {
+                    ResultSet keys = statement.getGeneratedKeys();
+                    if (keys != null && keys.next()) {
+                        value.setId_voluntario(keys.getInt(1));
+                    }
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                statement.close();
+                connection.close();
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return value;
+    }
+
+    @Override
+    public Voluntario remove(Object key) {
+        Voluntario v = get(key);
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            PreparedStatement statement = connection.prepareStatement(DELETE_VOLUNTARIO);
+
+            statement.setInt(1, (int) key);
+
+            try {
+                statement.executeUpdate();
+            } finally {
+                statement.close();
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return v;
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Voluntario> m) {
+        for (Voluntario v : m.values()) {
+            put(v.getId_voluntario(),v);
+        }
+    }
+
+    @Override
+    public void clear() {
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(DELETE_VOLUNTARIOS);
+            } finally {
+                connection.close();
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        Set<Integer> s = new HashSet<>();
+
+        try {
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+            PreparedStatement statement = connection.prepareStatement(SELECT_IDS);
+
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) {
+                    s.add(result.getInt(1));
+                }
+            } finally {
+                statement.close();
+                connection.close();
+            }
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return s;
+    }
+
+    @Override
+    public Collection<Voluntario> values() {
+        ArrayList<Voluntario> r = new ArrayList<>();
+        try {
+            Voluntario voluntario;
+
+            Connection connection = DriverManager.getConnection(url,user,password);
+            PreparedStatement statement = connection.prepareStatement(SELECT_VOLUNTARIOS);
+
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) {
+                    voluntario = new Voluntario();
+
+                    voluntario.setId_voluntario(result.getInt("id_voluntario"));
+                    voluntario.setNome_voluntario(result.getString("nome_voluntario"));
+                    voluntario.setData_nascimento(result.getString("data_nascimento"));
+                    voluntario.setProfissao(result.getString("profissao"));
+                    voluntario.setMorada(result.getString("morada"));
+                    voluntario.setCodigo_postal(result.getString("codigo_postal"));
+                    voluntario.setLocalidade(result.getString("localidade"));
+                    voluntario.setTelefone(result.getString("telefone"));
+                    voluntario.setTelemovel(result.getString("telemovel"));
+                    voluntario.setEmail(result.getString("email"));
+                    voluntario.setHab_academ(result.getString("hab_academ"));
+                    voluntario.setConhec_lingui(result.getString("conhec_lingui"));
+                    voluntario.setForm_compl(result.getString("form_compl"));
+                    voluntario.setExp_voluntariado(result.getString("exp_voluntariado"));
+                    voluntario.setConhec_constr(result.getString("conhec_constr"));
+                    voluntario.setTrabalho_grupo(result.getString("trabalho_grupo"));
+                    voluntario.setPub(result.getString("pub"));
+                    voluntario.setDisponi_tempo(result.getString("disponi_tempo"));
+                    voluntario.setCca_habitat(result.getString("cca_habitat"));
+
+                    r.add(voluntario);
+                }
+            }  finally {
+                statement.close();
+                connection.close();
+            }
+
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return r;
+    }
+
+    @Override
+    public Set<Entry<Integer, Voluntario>> entrySet() {
+        return null;
+    }
+}
