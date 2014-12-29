cec3714e21b450ca0200686e2928efc9fa285eb7
Duozhasht <tiago.m.oliveira@me.com>
2014-12-29 16:26:07 +0000

    Voluntario - Data Reduction

--- habitat-portugal/src/Model/Voluntario.java
+++ habitat-portugal/src/Model/Voluntario.java
@@ -1,74 +1,44 @@
 package Model;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 /**
- * @author davide on 18/12/14.
+ * @author Tiago on 18/12/14.
  */
 
 @SuppressWarnings("UnusedDeclaration")
 public class Voluntario {
     private int id_voluntario = -1;
-    private String nome_voluntario;
-    private String data_nascimento;
-    private String profissao;
-    private String morada;
-    private String codigo_postal;
-    private String localidade;
-    private String telefone;
-    private String telemovel;
-    private String email;
-    private String hab_academ;
-    private String conhec_lingui;
-    private String form_compl;
-    private String exp_voluntariado;
-    private String conhec_constr;
-    private String trabalho_grupo;
-    private String pub;
-    private String disponi_tempo;
-    private String cca_habitat;
+    private SimpleStringProperty nome_voluntario;
+    private SimpleStringProperty data_nascimento;
+    private SimpleStringProperty profissao;
+    private SimpleStringProperty morada;
+    private SimpleStringProperty contacto;
+    private SimpleStringProperty hab_academ;
+    private SimpleStringProperty conhec_lingui;
+    private SimpleStringProperty exp_voluntariado;
+    private SimpleBooleanProperty conhec_constr;
+    private SimpleBooleanProperty trabalho_grupo;
+    private SimpleBooleanProperty pub;
+    private SimpleStringProperty disponi_tempo;
+    private SimpleStringProperty cca_habitat;
 
-    public Voluntario() {
-        this.nome_voluntario = "";
-        this.data_nascimento = "";
-        this.profissao = "";
-        this.morada = "";
-        this.codigo_postal = "";
-        this.localidade = "";
-        this.telefone = "";
-        this.telemovel = "";
-        this.email = "";
-        this.hab_academ = "";
-        this.conhec_lingui = "";
-        this.form_compl = "";
-        this.exp_voluntariado = "";
-        this.conhec_constr = "";
-        this.trabalho_grupo = "";
-        this.pub = "";
-        this.disponi_tempo = "";
-        this.cca_habitat = "";
-    }
+    public Voluntario(){
+        this.nome_voluntario = new SimpleStringProperty();
+        this.data_nascimento = new SimpleStringProperty();
+        this.profissao = new SimpleStringProperty();
+        this.morada = new SimpleStringProperty();
+        this.contacto = new SimpleStringProperty();
+        this.hab_academ = new SimpleStringProperty();
+        this.conhec_lingui = new SimpleStringProperty();
+        this.exp_voluntariado = new SimpleStringProperty();
+        this.conhec_constr = new SimpleBooleanProperty();
+        this.trabalho_grupo = new SimpleBooleanProperty();
+        this.pub = new SimpleBooleanProperty();
+        this.disponi_tempo = new SimpleStringProperty();
+        this.cca_habitat = new SimpleStringProperty();
 
-    public Voluntario(String nome_voluntario, String data_nascimento, String profissao, String morada,
-                      String codigo_postal, String localidade, String telefone, String telemovel, String email,
-                      String hab_academ, String conhec_lingui, String form_compl, String exp_voluntariado,
-                      String conhec_constr, String trabalho_grupo, String pub, String disponi_tempo, String cca_habitat) {
-        this.nome_voluntario = nome_voluntario;
-        this.data_nascimento = data_nascimento;
-        this.profissao = profissao;
-        this.morada = morada;
-        this.codigo_postal = codigo_postal;
-        this.localidade = localidade;
-        this.telefone = telefone;
-        this.telemovel = telemovel;
-        this.email = email;
-        this.hab_academ = hab_academ;
-        this.conhec_lingui = conhec_lingui;
-        this.form_compl = form_compl;
-        this.exp_voluntariado = exp_voluntariado;
-        this.conhec_constr = conhec_constr;
-        this.trabalho_grupo = trabalho_grupo;
-        this.pub = pub;
-        this.disponi_tempo = disponi_tempo;
-        this.cca_habitat = cca_habitat;
     }
 
     public int getId_voluntario() {
@@ -80,172 +50,158 @@ public class Voluntario {
     }
 
     public String getNome_voluntario() {
+        return nome_voluntario.get();
+    }
+
+    public SimpleStringProperty nome_voluntarioProperty() {
         return nome_voluntario;
     }
 
     public void setNome_voluntario(String nome_voluntario) {
-        this.nome_voluntario = nome_voluntario;
+        this.nome_voluntario.set(nome_voluntario);
     }
 
     public String getData_nascimento() {
+        return data_nascimento.get();
+    }
+
+    public SimpleStringProperty data_nascimentoProperty() {
         return data_nascimento;
     }
 
     public void setData_nascimento(String data_nascimento) {
-        this.data_nascimento = data_nascimento;
+        this.data_nascimento.set(data_nascimento);
     }
 
     public String getProfissao() {
+        return profissao.get();
+    }
+
+    public SimpleStringProperty profissaoProperty() {
         return profissao;
     }
 
     public void setProfissao(String profissao) {
-        this.profissao = profissao;
+        this.profissao.set(profissao);
     }
 
     public String getMorada() {
+        return morada.get();
+    }
+
+    public SimpleStringProperty moradaProperty() {
         return morada;
     }
 
     public void setMorada(String morada) {
-        this.morada = morada;
+        this.morada.set(morada);
     }
 
-    public String getCodigo_postal() {
-        return codigo_postal;
+    public String getContacto() {
+        return contacto.get();
     }
 
-    public void setCodigo_postal(String codigo_postal) {
-        this.codigo_postal = codigo_postal;
+    public SimpleStringProperty contactoProperty() {
+        return contacto;
     }
 
-    public String getLocalidade() {
-        return localidade;
-    }
-
-    public void setLocalidade(String localidade) {
-        this.localidade = localidade;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getTelemovel() {
-        return telemovel;
-    }
-
-    public void setTelemovel(String telemovel) {
-        this.telemovel = telemovel;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setContacto(String contacto) {
+        this.contacto.set(contacto);
     }
 
     public String getHab_academ() {
+        return hab_academ.get();
+    }
+
+    public SimpleStringProperty hab_academProperty() {
         return hab_academ;
     }
 
     public void setHab_academ(String hab_academ) {
-        this.hab_academ = hab_academ;
+        this.hab_academ.set(hab_academ);
     }
 
     public String getConhec_lingui() {
+        return conhec_lingui.get();
+    }
+
+    public SimpleStringProperty conhec_linguiProperty() {
         return conhec_lingui;
     }
 
     public void setConhec_lingui(String conhec_lingui) {
-        this.conhec_lingui = conhec_lingui;
-    }
-
-    public String getForm_compl() {
-        return form_compl;
-    }
-
-    public void setForm_compl(String form_compl) {
-        this.form_compl = form_compl;
+        this.conhec_lingui.set(conhec_lingui);
     }
 
     public String getExp_voluntariado() {
+        return exp_voluntariado.get();
+    }
+
+    public SimpleStringProperty exp_voluntariadoProperty() {
         return exp_voluntariado;
     }
 
     public void setExp_voluntariado(String exp_voluntariado) {
-        this.exp_voluntariado = exp_voluntariado;
+        this.exp_voluntariado.set(exp_voluntariado);
     }
 
-    public String getConhec_constr() {
+    public boolean getConhec_constr() {
+        return conhec_constr.get();
+    }
+
+    public SimpleBooleanProperty conhec_constrProperty() {
         return conhec_constr;
     }
 
-    public void setConhec_constr(String conhec_constr) {
-        this.conhec_constr = conhec_constr;
+    public void setConhec_constr(boolean conhec_constr) {
+        this.conhec_constr.set(conhec_constr);
     }
 
-    public String getTrabalho_grupo() {
+    public boolean getTrabalho_grupo() {
+        return trabalho_grupo.get();
+    }
+
+    public SimpleBooleanProperty trabalho_grupoProperty() {
         return trabalho_grupo;
     }
 
-    public void setTrabalho_grupo(String trabalho_grupo) {
-        this.trabalho_grupo = trabalho_grupo;
+    public void setTrabalho_grupo(boolean trabalho_grupo) {
+        this.trabalho_grupo.set(trabalho_grupo);
     }
 
-    public String getPub() {
+    public boolean getPub() {
+        return pub.get();
+    }
+
+    public SimpleBooleanProperty pubProperty() {
         return pub;
     }
 
-    public void setPub(String pub) {
-        this.pub = pub;
+    public void setPub(boolean pub) {
+        this.pub.set(pub);
     }
 
     public String getDisponi_tempo() {
+        return disponi_tempo.get();
+    }
+
+    public SimpleStringProperty disponi_tempoProperty() {
         return disponi_tempo;
     }
 
     public void setDisponi_tempo(String disponi_tempo) {
-        this.disponi_tempo = disponi_tempo;
+        this.disponi_tempo.set(disponi_tempo);
     }
 
     public String getCca_habitat() {
+        return cca_habitat.get();
+    }
+
+    public SimpleStringProperty cca_habitatProperty() {
         return cca_habitat;
     }
 
     public void setCca_habitat(String cca_habitat) {
-        this.cca_habitat = cca_habitat;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Voluntario{");
-        sb.append("id_voluntario=").append(id_voluntario);
-        sb.append(", nome_voluntario='").append(nome_voluntario).append('\'');
-        sb.append(", data_nascimento='").append(data_nascimento).append('\'');
-        sb.append(", profissao='").append(profissao).append('\'');
-        sb.append(", morada='").append(morada).append('\'');
-        sb.append(", codigo_postal='").append(codigo_postal).append('\'');
-        sb.append(", localidade='").append(localidade).append('\'');
-        sb.append(", telefone='").append(telefone).append('\'');
-        sb.append(", telemovel='").append(telemovel).append('\'');
-        sb.append(", email='").append(email).append('\'');
-        sb.append(", hab_academ='").append(hab_academ).append('\'');
-        sb.append(", conhec_lingui='").append(conhec_lingui).append('\'');
-        sb.append(", form_compl='").append(form_compl).append('\'');
-        sb.append(", exp_voluntariado='").append(exp_voluntariado).append('\'');
-        sb.append(", conhec_constr='").append(conhec_constr).append('\'');
-        sb.append(", trabalho_grupo='").append(trabalho_grupo).append('\'');
-        sb.append(", pub='").append(pub).append('\'');
-        sb.append(", disponi_tempo='").append(disponi_tempo).append('\'');
-        sb.append(", cca_habitat='").append(cca_habitat).append('\'');
-        sb.append('}');
-        return sb.toString();
+        this.cca_habitat.set(cca_habitat);
     }
 }
